c59567e84fec81e875119a1c540be99f1f8717e9
epsortsdev <esportsdev@esports.ad.uci.edu>
2018-05-19 09:55:39 -0700

    Main menu functionality.

---
+++ Assets/Scripts/MainMenuMethods.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuMethods : MonoBehaviour
+{
+    #region Variables
+
+    #endregion
+
+    #region Properties
+
+    #endregion
+
+    #region Events
+    // Awake is called before Start
+    private void Awake()
+    {
+
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+    #endregion
+
+    #region Methods
+    // Start the game
+    public void StartGame()
+    {
+
+    }
+
+    // Quit the game
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    #endregion
+
+    #region Coroutines
+
+    #endregion
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    #region Variables
+    [SerializeField]
+    private GameObject[] screens;   // The different menu screens
+    #endregion
+
+    #region Properties
+
+    #endregion
+
+    #region Events
+    // Awake is called before Start
+    private void Awake()
+    {
+        ChangeScreen(0);
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+    #endregion
+
+    #region Methods
+    // Change the menu screen
+    public void ChangeScreen(int index)
+    {
+        for (int i = 0; i < screens.Length; ++i)
+        {
+            screens[i].SetActive(i == index);
+        }
+    }
+    #endregion
+
+    #region Coroutines
+
+    #endregion
+}
