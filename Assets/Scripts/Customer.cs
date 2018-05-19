9284c83030b77e39521371b63e15af24a40f6c0c
epsortsdev <esportsdev@esports.ad.uci.edu>
2018-05-19 12:01:55 -0700

    Some more scripts.

---
+++ Assets/Scripts/Customer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Customer : MonoBehaviour
+{
+    #region Variables
+
+    #endregion
+
+    #region Properties
+
+    #endregion
+
+    #region Events
+    // Awake is called before Start
+    private void Awake()
+    {
+
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+    #endregion
+
+    #region Methods
+
+    #endregion
+
+    #region Coroutines
+
+    #endregion
+}
