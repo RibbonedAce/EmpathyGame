fcb4c97f650912a4a1a89d5de35b0662d1f02548
epsortsdev <esportsdev@esports.ad.uci.edu>
2018-05-19 11:47:29 -0700

    Money prefabs.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    #region Variables
+    
+    #endregion
+
+    #region Properties
+    public int Score { get; private set; }  // The score of the game
+    #endregion
+
+    #region Events
+    // Awake is called before Start
+    private void Awake()
+    {
+
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+    #endregion
+
+    #region Methods
+
+    #endregion
+
+    #region Coroutines
+
+    #endregion
+}
---
+++ Assets/Scripts/Money.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Money : MonoBehaviour
+{
+    #region Variables
+    [SerializeField]
+    private int value;                          // How much the money's worth
+    #endregion
+
+    #region Properties
+    public int Value { get { return value; } }  // Returns value
+    #endregion
+
+    #region Events
+    // Awake is called before Start
+    private void Awake()
+    {
+
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+    #endregion
+
+    #region Methods
+
+    #endregion
+
+    #region Coroutines
+
+    #endregion
+}
