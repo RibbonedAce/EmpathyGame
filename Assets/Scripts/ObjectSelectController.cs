52d74043a72b0187d1bf515a2671d0276e7f42b0
epsortsdev <esportsdev@esports.ad.uci.edu>
2018-05-19 15:27:12 -0700

    added money pickup and area calculating value

---
+++ Assets/Scripts/ObjectSelectController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSelectController : MonoBehaviour {
+    Vector3 RayCastDir;
+    Vector2 Offset;
+    GameObject SelectedObject;
+    float distanceFromCamera;
+    
+	// Use this for initialization
+	void Start () {
+        SelectedObject = null;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(SelectedObject == null)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit2D hitList = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), RayCastDir);
+                if (hitList && hitList.transform.GetComponents<PickUpObject>().Length > 0)
+                {
+                    SelectedObject = hitList.transform.gameObject;
+                    Offset = new Vector2(hitList.transform.position.x, hitList.transform.position.y)- hitList.point;
+                }
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                SelectedObject.transform.position = ray.GetPoint(distanceFromCamera) + new Vector3(Offset.x,Offset.y);
+            }
+            else
+            {
+                SelectedObject = null;
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/PickUpObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PickUpObject : MonoBehaviour {
+    [SerializeField]
+    int _value;
+    bool IsSelected;
+    public int Value
+    {
+        get
+        {
+            return _value;
+        }
+    }
+    public abstract int GetMonetaryValue
+    {
+        get;
+    }
+    public bool GetSetSelected
+    {
+        get
+        {
+            return IsSelected;
+        }
+        set
+        {
+            IsSelected = value;
+        }
+    }
+}
